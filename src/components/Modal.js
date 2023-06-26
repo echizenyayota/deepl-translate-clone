2574aa90005469e5078ed873a4d90e0ecfe2a378
echizenyayota <virtualmisso30@gmail.com>
2023-06-26 13:29:31 +0900

    38:01 choose the source-language with Select Element

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,40 @@
 const Modal = () => {
   return (
-    <div>Modal</div>
+    <div className="option-list">
+      <div className="search-bar">
+        <select className="source-language">
+          <option value="BG">BG</option>
+          <option value="CS">CS</option>
+          <option value="DA">DA</option>
+          <option value="DE">DE</option>
+          <option value="EL">EL</option>
+          <option value="EN">EN</option>
+          <option value="ES">ES</option>
+          <option value="ET">ET</option>
+          <option value="FI">FI</option>
+          <option value="FR">FR</option>
+          <option value="HU">HU</option>
+          <option value="ID">ID</option>
+          <option value="IT">IT</option>
+          <option value="JA">JA</option>
+          <option value="KO">KO</option>
+          <option value="LT">LT</option>
+          <option value="LV">LV</option>
+          <option value="NB">NB</option>
+          <option value="NL">NL</option>
+          <option value="PL">PL</option>
+          <option value="PT">PT</option>
+          <option value="RO">RO</option>
+          <option value="RU">RU</option>
+          <option value="SK">SK</option>
+          <option value="SL">SL</option>
+          <option value="SV">SV</option>
+          <option value="TR">TR</option>
+          <option value="UK">UK</option>
+          <option value="ZH">ZH</option>
+        </select>
+      </div>
+    </div>
   )
 }
 
