edaf15fe8efc4de772838adf15cf66caf513d10c
echizenyayota <virtualmisso30@gmail.com>
2023-06-29 18:55:26 +0900

    1:25:59 set the API_KEY and the API_HOST in the server.js

--- server.js
+++ server.js
@@ -4,8 +4,34 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express()
+const app = express();
 
 app.use(cors());
 
-app.listen(PORT, () => console.log("Server running on PORT " + PORT));
+app.get("/translation", async(req, res) => {
+
+  const { outputLanguage, inputLanguage, textToTranslate } = req.query;
+
+  const options = {
+    method: "GET",
+    params: {
+      target: outputLanguage,
+      source: inputLanguage,
+      text: textToTranslate
+    },
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': process.env.RAPID_API_HOST
+    }
+  }
+
+  try {
+    const response = await axios("https://deepl-translate-machine.p.rapidapi.com/v1/translate/text", options);
+    res.status(200).json(response.data.data.texts[0].text);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ message: err });
+  }
+});
+
+app.listen(PORT, () => console.log("Server running on PORT " + PORT))
